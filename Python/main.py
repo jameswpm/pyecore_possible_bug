7e2e2f777cc600fe2f44cc516d9c2f2d9c6d60b6
James Miranda <jameswpm@gmail.com>
2023-10-16 13:05:54 +0200

    Include the proposed workaround mentioned in the original Issue

--- Python/main.py
+++ Python/main.py
@@ -2,7 +2,16 @@ from os import path as osp
 from pathlib import Path
 
 #PyEcore
-from pyecore.resources import ResourceSet, URI
+from pyecore.resources import ResourceSet, URI, AbstractURIConverter
+
+class FileSpecificProtocolURIConverter(AbstractURIConverter):
+    @staticmethod
+    def can_handle(uri):
+        return uri.protocol == 'file'
+
+    @staticmethod
+    def convert(uri):
+        return URI(osp.abspath(uri.plain.replace('file:/', '')))
 
 RESOURCES_PATH = osp.join(Path(__file__).parent)
 
@@ -10,12 +19,15 @@ ecore_path = osp.join(RESOURCES_PATH, '..', 'A.ecore')
 xmi_path = osp.join(RESOURCES_PATH, '..', 'BaseClass.xmi')
 
 resource_set = ResourceSet()
+resource_set.uri_converter.append(FileSpecificProtocolURIConverter)
 #register metamodel
 resource_path = resource_set.get_resource(URI(ecore_path))
 content = resource_path.contents[0]
 resource_set.metamodel_registry[content.nsURI] = content
 #register model
 m_resource = resource_set.get_resource(URI(xmi_path))
+
+
 
 #iterate the model and print the class name
 for element in m_resource.contents:    
@@ -24,5 +36,3 @@ for element in m_resource.contents:
     object_element = element.object
     object_element_class = object_element.eClass.name
     print(f'Reference Class {object_element_class} with attribute {object_element.eGet("simpleAttr")}')
-
-
